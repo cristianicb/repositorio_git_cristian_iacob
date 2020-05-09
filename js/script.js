85409a260cf7e0dc286ccf368390e9a6e8ff6ba5
Cristian Iacob <iacobtudorcristian@gmail.com>
2020-05-09 18:48:28 +0200

    primer estilo

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
 
   $("#search").click(function(){
-    let name = window.prompt("search", "Text");
-    window.open('http://google.com/search?q='+name);
+    let name = window.prompt("search", "Nasa");
+
+    if (name === null) {
+        return; //break out of the function early
+    }
+    else{
+      window.open('http://google.com/search?q='+name);
+    }
   });
 
 
+
+if ($(window).width() > 1300) {
+  $(".header-right").removeClass("hidden");
+} else {
   $(".menu").click(function(){
-    $(".header-right").toggleClass("closed");
+    $(".header-right").toggleClass("hidden");
   });
+}
+
+
 
 });
